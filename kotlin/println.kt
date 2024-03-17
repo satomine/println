33ec6afdc16b5ee242a9beaf451431e1aabefb21
satomine <satomine0401@gmail.com>
2024-03-17 21:52:12 +0900

    Implement in Kotlin

---
+++ kotlin/println.kt
@@ -0,0 +1,20 @@
+import kotlin.math.ln
+
+fun println(x: Number) {
+    print(ln(x.toDouble()))
+}
+
+fun main() {
+    println(1)
+    println()
+
+    println(2.0)
+    println()
+
+    println(0)
+    println()
+
+    println(-1)
+    println()
+}
+
